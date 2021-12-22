5be3908e4f27dc01e25aff32ee331c9e9fc7fca1
王建 <wangjianio@users.noreply.github.com>
2021-12-22 15:20:23 +0800

    feat: 公安备案

--- src/components/layout.js
+++ src/components/layout.js
@@ -66,10 +66,14 @@ export default function Layout(props) {
         © {new Date().getFullYear()}, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
         {' '}
         <a href="https://beian.miit.gov.cn/" target="_blank">蒙ICP备17002783号</a>
-
-        <div style="width:300px;margin:0 auto; padding:20px 0;">
-          <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=15072502000017" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="http://www.beian.gov.cn/img/ghs.png" style="float:left;" /><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">蒙公网安备 15072502000017号</p></a>
-        </div>
+        {' '}
+        <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=15072502000017"
+          style={{ lineHeight: '20px' }}
+          >
+          <img src="http://www.beian.gov.cn/img/ghs.png" style={{ marginBottom: 0, verticalAlign: -4 }} />
+          {' '}
+          <span style={{ color: '#939393' }}>蒙公网安备 15072502000017号</span>
+        </a>
       </footer>
     </div>
   );
