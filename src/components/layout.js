5c430f9274febae1934a50fd2de2c05638ed0d17
王建 <wangjianio@users.noreply.github.com>
2021-12-22 14:40:51 +0800

    feat: 公安备案

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import { rhythm, scale } from "../utils/typography"
+import { rhythm, scale } from "../utils/typography";
 
 
 export default function Layout(props) {
@@ -29,7 +29,7 @@ export default function Layout(props) {
           {title}
         </Link>
       </h1>
-    )
+    );
   } else {
     header = (
       <h3
@@ -49,7 +49,7 @@ export default function Layout(props) {
           {title}
         </Link>
       </h3>
-    )
+    );
   }
   return (
     <div
@@ -66,7 +66,11 @@ export default function Layout(props) {
         © {new Date().getFullYear()}, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
         {' '}
         <a href="https://beian.miit.gov.cn/" target="_blank">蒙ICP备17002783号</a>
+
+        <div style="width:300px;margin:0 auto; padding:20px 0;">
+          <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=15072502000017" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="http://www.beian.gov.cn/img/ghs.png" style="float:left;" /><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">蒙公网安备 15072502000017号</p></a>
+        </div>
       </footer>
     </div>
-  )
+  );
 }
